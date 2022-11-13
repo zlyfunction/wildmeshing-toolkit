07aadc9dad966cf865b990e503a2fe895d94405b
zlyfunction <zhuleyi001@126.com>
2022-11-13 17:40:25 -0500

    add json support

---
+++ app/extreme_opt/test_json.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "json.hpp"
+#include <fstream>
+
+using json = nlohmann::json;
+
+int main()
+{
+    std::ifstream f("example.json");
+    json data = json::parse(f);
+    std::cout << data["data"][0];
+    double E_avg = 12.0, E_max = 209.3;
+    data["data"].push_back({{"E_avg", E_avg}, {"E_max", E_max}});
+
+    std::ofstream fout("example_out.json");
+    fout << std::setw(4) << data << std::endl;
+}
