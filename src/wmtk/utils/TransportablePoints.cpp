b339c840ec049ee8f2268c706eb4bd4a078888a7
Michael Tao <mtao@dgp.toronto.edu>
2023-01-15 17:03:30 -0500

    transportable points builds

--- src/wmtk/utils/TransportablePoints.cpp
+++ src/wmtk/utils/TransportablePoints.cpp
@@ -1,4 +1,3 @@
-
 #include <wmtk/utils/TransportablePoints.hpp>
 
 
@@ -12,9 +11,9 @@ void TransportablePointsBase::before_hook(const TriMesh& m, const std::set<size_
     std::set<size_t>& active_pts = active_points.local();
     // go through set of input tris and try to move every point in an output tri
     for (size_t point_index = 0; point_index < triangle_indices.size(); ++point_index) {
-        if (input_tris.find(index) != input_tris.end()) {
+        if (input_tris.find(point_index) != input_tris.end()) {
             active_pts.emplace(point_index);
-            update_barycetnric(m, point_index);
+            update_global_coordinate(m, point_index);
         }
     }
 }
@@ -23,21 +22,29 @@ void TransportablePointsBase::after_hook(const TriMesh& m, const std::set<size_t
 {
     // go through set of input tris and try to move every point in an output tri
     for (const size_t point_index : active_points.local()) {
-        // this point needs to be moved forward in this operation
-        // try to see if it's in a triangle
-        bool found = false;
-        for (const size_t output_tri : output_tris) {
-            if (point_in_triangle(m, triangle_index, point_index)) {
-                triangle_indices[point_index] = triangle_index;
-                barycentric_coordinates[point_index] =
-                    get_barycentric(m, triangle_index, point_index);
-                found = true;
-            }
-        }
-
-        if (!found) {
-            spdlog::warn("Point not found in a triangle, backup mechanism required");
-        }
+        update_local_coordinate(m, point_index, output_tris);
     }
 }
 
+
+// derived class is required to store a global representation of the point, used in before_hook
+void TransportablePointsBase::update_local_coordinate(
+    const TriMesh& m,
+    size_t point_index,
+    const std::set<size_t>& possible_tris)
+{
+    // this point needs to be moved forward in this operation
+    // try to see if it's in a triangle
+    bool found = false;
+    for (const size_t triangle_index : possible_tris) {
+        if (point_in_triangle(m, triangle_index, point_index)) {
+            triangle_indices[point_index] = triangle_index;
+            barycentric_coordinates[point_index] = get_barycentric(m, triangle_index, point_index);
+            found = true;
+        }
+    }
+
+    if (!found) {
+        spdlog::warn("Point not found in a triangle, backup mechanism required");
+    }
+}
--- src/wmtk/utils/TransportablePoints.hpp
+++ src/wmtk/utils/TransportablePoints.hpp
@@ -1,19 +1,28 @@
 #pragma once
 
+#define USE_CALLBACK_FOR_TRANSPOORTABLE_POINTS
 
 #include <wmtk/TriMesh.h>
+#include <functional>
 
 
 namespace wmtk {
 class TransportablePointsBase
 {
-    ~TransportablePointsBase();
+public:
+    virtual ~TransportablePointsBase();
     // convenience function that just calls
     void before_hook(const TriMesh& m, const std::set<size_t>& input_tris);
     void after_hook(const TriMesh& m, const std::set<size_t>& output_tris);
 
     // derived class is required to store a global representation of the point, used in before_hook
-    virtual void apply_barycentric(const TriMesh& m, size_t point_index) = 0;
+    virtual void update_global_coordinate(const TriMesh& m, size_t point_index) = 0;
+
+    // derived class is required to store a global representation of the point, used in before_hook
+    void update_local_coordinate(
+        const TriMesh& m,
+        size_t point_index,
+        const std::set<size_t>& possible_tris);
 
     // derived class is required to identify which point and triangle
     virtual bool point_in_triangle(const TriMesh& m, size_t triangle_index, size_t point_index)
@@ -23,43 +32,88 @@ class TransportablePointsBase
     get_barycentric(const TriMesh& m, size_t triangle_index, size_t point_index) const = 0;
 
 
-    void update_local_coordinates(
-        const TriMesh& m,
-        const std::set<size_t>& input_tris,
-        const std::set<size_t>& output_tris);
-
 protected:
     // local representation of points in a triangle mesh
     tbb::concurrent_vector<size_t> triangle_indices;
     tbb::concurrent_vector<std::array<double, 3>> barycentric_coordinates;
 
     tbb::enumerable_thread_specific<std::set<size_t>> active_points;
+
+    tbb::concurrent_vector<std::set<size_t>> point_bins;
 };
 
 template <typename PointType>
 class TransportablePoints
 {
-    // derived class is required to store a global representation of the point, used in before_hook
-    void apply_barycentric(const TriMesh& m, size_t point_index) override;
+public:
+    // derived class is required to store a global representation of the point, used in
+    // before_hook
+    void update_global_coordinate(const TriMesh& m, size_t point_index) override;
 
-    // derived class is required to identify which point and triangle
+    // predicate to determine whether a point lies in a particular triangle
     bool point_in_triangle(const TriMesh& m, size_t triangle_index, size_t point_index)
         const override;
 
+    // computes the barycentric coordinates for the point at point_index assuming that it lies in
+    // triangle_index
     std::array<double, 3>
     get_barycentric(const TriMesh& m, size_t triangle_index, size_t point_index) const override;
 
 
+#if defined(USE_CALLBACK_FOR_TRANSPOORTABLE_POINTS)
+
+    using BarycentricInterpFuncType = std::function<PointType(
+        const TriMesh&,
+        const std::array<double, 3>&,
+        const std::array<std::reference_wrapper<const PointType>, 3>&)>;
+    using PointInTriangleFuncType = std::function<bool(const TriMesh&, size_t, size_t)>;
+    using GetBarycentricFuncType =
+        std::function<std::array<double, 3>(const TriMesh&, size_t, size_t)>;
+
+    BarycentricInterpFuncType barycentric_interp_callback;
+    PointInTriangleFuncType point_in_triangle_callback;
+    GetBarycentricFuncType get_barycentric_callback;
+#endif
     // global coordinates
     tbb::concurrent_vector<PointType> points_global;
 };
 
+#if defined(USE_CALLBACK_FOR_TRANSPOORTABLE_POINTS)
+template <typename PointType>
+void TransportablePoints<PointType>::update_global_coordinate(const TriMesh& m, size_t point_index)
+{
+    const std::array<size_t, 3>& vertex_indices = m.m_tri_connectivity[point_index].m_indices;
+    const tbb::concurrent_vector<PointType>& P =
+        dynamic_cast<const AttributeCollection<PointType>&>(&m.p_vertex_attrs).m_attributes;
+    const std::array<std::reference_wrapper<const PointType>, 3> points{
+        {P[vertex_indices[0]], P[vertex_indices[1]], P[vertex_indices[2]]}};
+
+    points_global[point_index] = barycentric_interp_callback(
+        m,
+        TransportablePointsBase::triangle_indices[point_index],
+        TransportablePointsBase::barycentric_coordinates[point_index],
+        points);
+}
 
 template <typename PointType>
-void TransportablePoints<PointType>::global_from_local(const TriMesh& m)
-{}
+bool TransportablePoints<PointType>::point_in_triangle(
+    const TriMesh& m,
+    size_t triangle_index,
+    size_t point_index) const
+{
+    return point_in_triangle_callback(m, triangle_index, point_index);
+}
+
 template <typename PointType>
-void TransportablePoints<PointType>::local_from_global(const TriMesh& m)
-{}
+std::array<double, 3> TransportablePoints<PointType>::get_barycentric(
+    const TriMesh& m,
+    size_t triangle_index,
+    size_t point_index) const
+{
+    return get_barycentric_callback(m, triangle_index, point_index);
+}
+
+#endif
+
 
 } // namespace wmtk
