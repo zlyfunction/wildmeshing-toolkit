8d2a34fa7dd48973a51a70f5a006335e26e894b4
zlyfunction <zhuleyi001@126.com>
2025-10-17 17:46:39 -0400

    change libigl

--- cmake/libigl.cmake
+++ cmake/libigl.cmake
@@ -6,6 +6,6 @@ include(FetchContent)
 FetchContent_Declare(
     libigl
     GIT_REPOSITORY https://github.com/zlyfunction/libigl.git
-    GIT_TAG 6bc3b401e774a3efd88a0a0ea7fda2d87c3f0f55
+    GIT_TAG c0b9197c858471a49177486584418791ef1b5cde
 )
 FetchContent_MakeAvailable(libigl)
